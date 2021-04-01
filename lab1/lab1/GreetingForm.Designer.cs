0dde7fb4fd757669f31e7060b351a16ac2fdd359
Alex <bossalex9@yahoo.com>
2021-04-02 00:40:56 +0300

    lab1

--- lab1/lab1/GreetingForm.Designer.cs
+++ lab1/lab1/GreetingForm.Designer.cs
@@ -34,11 +34,19 @@
             this.gridEchipaj = new System.Windows.Forms.DataGridView();
             this.labelEchipaj = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.gridPirati = new System.Windows.Forms.DataGridView();
+            this.gridMarinar = new System.Windows.Forms.DataGridView();
             this.labelPirati = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.deletePirat = new System.Windows.Forms.Button();
+            this.updateMarinar = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.refresh = new System.Windows.Forms.Button();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.adauga = new System.Windows.Forms.Button();
+            this.cod_echipajText = new lab1.WaterMarkTextBox();
+            this.cod_individText = new lab1.WaterMarkTextBox();
+            this.aliasText = new lab1.WaterMarkTextBox();
+            this.functieText = new lab1.WaterMarkTextBox();
+            this.cod_marinarText = new lab1.WaterMarkTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.MainSplitter)).BeginInit();
             this.MainSplitter.Panel1.SuspendLayout();
             this.MainSplitter.Panel2.SuspendLayout();
@@ -46,7 +54,7 @@
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridEchipaj)).BeginInit();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gridPirati)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridMarinar)).BeginInit();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -70,7 +78,7 @@
             // 
             this.MainSplitter.Panel2.Controls.Add(this.panel2);
             this.MainSplitter.Panel2.Controls.Add(this.labelPirati);
-            this.MainSplitter.Size = new System.Drawing.Size(845, 411);
+            this.MainSplitter.Size = new System.Drawing.Size(836, 411);
             this.MainSplitter.SplitterDistance = 205;
             this.MainSplitter.SplitterWidth = 7;
             this.MainSplitter.TabIndex = 0;
@@ -83,7 +91,7 @@
             this.panel1.Controls.Add(this.gridEchipaj);
             this.panel1.Location = new System.Drawing.Point(17, 41);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(812, 157);
+            this.panel1.Size = new System.Drawing.Size(803, 157);
             this.panel1.TabIndex = 2;
             // 
             // gridEchipaj
@@ -95,9 +103,10 @@
             this.gridEchipaj.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridEchipaj.Location = new System.Drawing.Point(0, 0);
             this.gridEchipaj.Name = "gridEchipaj";
+            this.gridEchipaj.ReadOnly = true;
             this.gridEchipaj.RowHeadersWidth = 51;
             this.gridEchipaj.RowTemplate.Height = 24;
-            this.gridEchipaj.Size = new System.Drawing.Size(809, 154);
+            this.gridEchipaj.Size = new System.Drawing.Size(800, 154);
             this.gridEchipaj.TabIndex = 1;
             // 
             // labelEchipaj
@@ -115,25 +124,27 @@
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.Controls.Add(this.gridPirati);
+            this.panel2.Controls.Add(this.gridMarinar);
             this.panel2.Location = new System.Drawing.Point(17, 37);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(812, 146);
+            this.panel2.Size = new System.Drawing.Size(803, 131);
             this.panel2.TabIndex = 3;
             // 
-            // gridPirati
+            // gridMarinar
             // 
-            this.gridPirati.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.gridMarinar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.gridPirati.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.gridPirati.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridPirati.Location = new System.Drawing.Point(0, 0);
-            this.gridPirati.Name = "gridPirati";
-            this.gridPirati.RowHeadersWidth = 51;
-            this.gridPirati.RowTemplate.Height = 24;
-            this.gridPirati.Size = new System.Drawing.Size(809, 142);
-            this.gridPirati.TabIndex = 2;
+            this.gridMarinar.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridMarinar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridMarinar.Location = new System.Drawing.Point(0, 0);
+            this.gridMarinar.Name = "gridMarinar";
+            this.gridMarinar.ReadOnly = true;
+            this.gridMarinar.RowHeadersWidth = 51;
+            this.gridMarinar.RowTemplate.Height = 24;
+            this.gridMarinar.Size = new System.Drawing.Size(800, 127);
+            this.gridMarinar.TabIndex = 2;
+            this.gridMarinar.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridMarinar_CellClick);
             // 
             // labelPirati
             // 
@@ -141,27 +152,31 @@
             this.labelPirati.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelPirati.Location = new System.Drawing.Point(12, 5);
             this.labelPirati.Name = "labelPirati";
-            this.labelPirati.Size = new System.Drawing.Size(68, 29);
+            this.labelPirati.Size = new System.Drawing.Size(100, 29);
             this.labelPirati.TabIndex = 2;
-            this.labelPirati.Text = "Pirati";
+            this.labelPirati.Text = "Marinari";
             // 
-            // button1
+            // deletePirat
             // 
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 35);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Delete Pirat";
-            this.button1.UseVisualStyleBackColor = true;
+            this.deletePirat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deletePirat.Location = new System.Drawing.Point(3, 3);
+            this.deletePirat.Name = "deletePirat";
+            this.deletePirat.Size = new System.Drawing.Size(149, 35);
+            this.deletePirat.TabIndex = 1;
+            this.deletePirat.Text = "Delete Marinar";
+            this.deletePirat.UseVisualStyleBackColor = true;
+            this.deletePirat.Click += new System.EventHandler(this.deletePirat_Click);
             // 
-            // button2
+            // updateMarinar
             // 
-            this.button2.Location = new System.Drawing.Point(3, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(136, 26);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.updateMarinar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateMarinar.Location = new System.Drawing.Point(3, 41);
+            this.updateMarinar.Name = "updateMarinar";
+            this.updateMarinar.Size = new System.Drawing.Size(149, 35);
+            this.updateMarinar.TabIndex = 2;
+            this.updateMarinar.Text = "Update Marinar";
+            this.updateMarinar.UseVisualStyleBackColor = true;
+            this.updateMarinar.Click += new System.EventHandler(this.updateMarinar_Click);
             // 
             // panel3
             // 
@@ -169,18 +184,103 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel3.Controls.Add(this.button1);
-            this.panel3.Controls.Add(this.button2);
-            this.panel3.Location = new System.Drawing.Point(863, 5);
+            this.panel3.Controls.Add(this.adauga);
+            this.panel3.Controls.Add(this.cod_echipajText);
+            this.panel3.Controls.Add(this.cod_individText);
+            this.panel3.Controls.Add(this.aliasText);
+            this.panel3.Controls.Add(this.functieText);
+            this.panel3.Controls.Add(this.cod_marinarText);
+            this.panel3.Controls.Add(this.refresh);
+            this.panel3.Controls.Add(this.deletePirat);
+            this.panel3.Controls.Add(this.updateMarinar);
+            this.panel3.Location = new System.Drawing.Point(854, 5);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(146, 411);
+            this.panel3.Size = new System.Drawing.Size(159, 411);
             this.panel3.TabIndex = 3;
+            // 
+            // refresh
+            // 
+            this.refresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refresh.Location = new System.Drawing.Point(3, 123);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(149, 35);
+            this.refresh.TabIndex = 3;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // adauga
+            // 
+            this.adauga.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adauga.Location = new System.Drawing.Point(3, 82);
+            this.adauga.Name = "adauga";
+            this.adauga.Size = new System.Drawing.Size(149, 35);
+            this.adauga.TabIndex = 9;
+            this.adauga.Text = "Adauga Marinar";
+            this.adauga.UseVisualStyleBackColor = true;
+            this.adauga.Click += new System.EventHandler(this.adauga_Click);
+            // 
+            // cod_echipajText
+            // 
+            this.cod_echipajText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cod_echipajText.Location = new System.Drawing.Point(3, 300);
+            this.cod_echipajText.Name = "cod_echipajText";
+            this.cod_echipajText.Size = new System.Drawing.Size(149, 27);
+            this.cod_echipajText.TabIndex = 8;
+            this.cod_echipajText.WaterMarkColor = System.Drawing.Color.Gray;
+            this.cod_echipajText.WaterMarkText = "cod echipaj(int)";
+            // 
+            // cod_individText
+            // 
+            this.cod_individText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cod_individText.Location = new System.Drawing.Point(3, 267);
+            this.cod_individText.Name = "cod_individText";
+            this.cod_individText.Size = new System.Drawing.Size(149, 27);
+            this.cod_individText.TabIndex = 7;
+            this.cod_individText.WaterMarkColor = System.Drawing.Color.Gray;
+            this.cod_individText.WaterMarkText = "cod individ(int)";
+            // 
+            // aliasText
+            // 
+            this.aliasText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aliasText.Location = new System.Drawing.Point(3, 234);
+            this.aliasText.Name = "aliasText";
+            this.aliasText.Size = new System.Drawing.Size(149, 27);
+            this.aliasText.TabIndex = 6;
+            this.aliasText.WaterMarkColor = System.Drawing.Color.Gray;
+            this.aliasText.WaterMarkText = "alias(string)";
+            // 
+            // functieText
+            // 
+            this.functieText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.functieText.Location = new System.Drawing.Point(3, 201);
+            this.functieText.Name = "functieText";
+            this.functieText.Size = new System.Drawing.Size(149, 27);
+            this.functieText.TabIndex = 5;
+            this.functieText.WaterMarkColor = System.Drawing.Color.Gray;
+            this.functieText.WaterMarkText = "functie(string)";
+            // 
+            // cod_marinarText
+            // 
+            this.cod_marinarText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cod_marinarText.Location = new System.Drawing.Point(3, 168);
+            this.cod_marinarText.Name = "cod_marinarText";
+            this.cod_marinarText.Size = new System.Drawing.Size(149, 27);
+            this.cod_marinarText.TabIndex = 4;
+            this.cod_marinarText.WaterMarkColor = System.Drawing.Color.Gray;
+            this.cod_marinarText.WaterMarkText = "cod marinar(int)";
             // 
             // GreetingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1021, 428);
+            this.ClientSize = new System.Drawing.Size(1025, 428);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.MainSplitter);
             this.Name = "GreetingForm";
@@ -195,8 +295,9 @@
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gridEchipaj)).EndInit();
             this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.gridPirati)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridMarinar)).EndInit();
             this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -207,11 +308,19 @@
         private System.Windows.Forms.DataGridView gridEchipaj;
         private System.Windows.Forms.Label labelEchipaj;
         private System.Windows.Forms.Label labelPirati;
-        private System.Windows.Forms.DataGridView gridPirati;
+        private System.Windows.Forms.DataGridView gridMarinar;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button deletePirat;
+        private System.Windows.Forms.Button updateMarinar;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button refresh;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private WaterMarkTextBox cod_echipajText;
+        private WaterMarkTextBox cod_individText;
+        private WaterMarkTextBox aliasText;
+        private WaterMarkTextBox functieText;
+        private WaterMarkTextBox cod_marinarText;
+        private System.Windows.Forms.Button adauga;
     }
 }
--- lab1/lab1/GreetingForm.cs
+++ lab1/lab1/GreetingForm.cs
@@ -24,39 +24,148 @@ namespace lab1
             InitializeComponent();
         }
 
-        private void GreetingForm_Load(object sender, EventArgs e)
+        private void LoadData()
         {
             try
             {
-                using(SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
                     parentAdapter.SelectCommand = new SqlCommand("select * from Echipaj;", connection);
-                    // childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Jocuri", connection);
-                    childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Pirati", connection);
+                    childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Marinar", connection);
                     parentAdapter.Fill(ds, "Echipaj");
-                    childAdapter.Fill(ds, "Pirati");
+                    childAdapter.Fill(ds, "Marinar");
                     bsParent.DataSource = ds.Tables["Echipaj"];
                     gridEchipaj.DataSource = bsParent;
-                    // dataGridView1.DataSource = bsParent;
                     DataColumn pk = ds.Tables["Echipaj"].Columns["cod_echipaj"];
-                    DataColumn fk = ds.Tables["Pirati"].Columns["cod_echipaj"];
-                    // DataColumn pk = ds.Tables["SaliDeJoc"].Columns["id"];
-                    // DataColumn fk = ds.Tables["Jocuri"].Columns["cod_sala"];
-                    // DataRelation relation = new DataRelation("FK_SaliDeJoc_Jocuri", pk, fk);
-                    DataRelation relation = new DataRelation("FK_Echipaj_Pirati", pk, fk);
+                    DataColumn fk = ds.Tables["Marinar"].Columns["cod_echipaj"];
+                    DataRelation relation = new DataRelation("FK_Echipaj_Marinar", pk, fk);
                     ds.Relations.Add(relation);
                     bsChild.DataSource = bsParent;
-                    bsChild.DataMember = "FK_Echipaj_Pirati";
-                    gridPirati.DataSource = bsChild;
-                    // dataGridView2.DataSource = bsChild;
-                    // MessageBox.Show("M-am deschis ;)");
-
+                    bsChild.DataMember = "FK_Echipaj_Marinar";
+                    gridMarinar.DataSource = bsChild;
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void GreetingForm_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void deletePirat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var rows = gridMarinar.SelectedRows;
+                if (rows.Count == 0)
+                {
+                    MessageBox.Show("Please select a row!");
+                    return;
+                }
+
+                var id =(int) rows[0].Cells["cod_marinar"].Value;
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand deleteCommand = new SqlCommand("DELETE FROM Marinar WHERE cod_marinar=@cod;",
+                        connection);
+                    
+                    deleteCommand.Parameters.AddWithValue("@cod", id);
+                    int deleteCount = deleteCommand.ExecuteNonQuery();
+                    MessageBox.Show(("Numarul marinarilor stersi este: " + deleteCount));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void updateMarinar_Click(object sender, EventArgs e)
+        {
+            var rows = gridMarinar.SelectedRows;
+            if (cod_marinarText.Text.Equals(""))
+            {
+                MessageBox.Show("Please select a row or fill in the data!");
+                return;
+            }
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand updateCommand = new SqlCommand("UPDATE Marinar SET functie=@functie, alias=@alias,cod_individ=@cod_individ,cod_echipaj=@cod_echipaj WHERE" +
+                                                          " cod_marinar=@cod_marinar;", connection);
+                updateCommand.Parameters.AddWithValue("@functie", functieText.Text);
+                updateCommand.Parameters.AddWithValue("@alias", aliasText.Text);
+                updateCommand.Parameters.AddWithValue("@cod_individ", int.Parse(cod_individText.Text));
+                updateCommand.Parameters.AddWithValue("@cod_echipaj", int.Parse(cod_echipajText.Text));
+                updateCommand.Parameters.AddWithValue("@cod_marinar", int.Parse(cod_marinarText.Text));
+                int updateCount = updateCommand.ExecuteNonQuery();
+                MessageBox.Show("Numarul marinarilor actualizati: " + updateCount);
+            }
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            ds.Tables["Marinar"].Clear();
+            ds.Tables["Echipaj"].Clear();
+            cod_marinarText.Text = "";
+            functieText.Text = "";
+            aliasText.Text = "";
+            cod_individText.Text = "";
+            cod_echipajText.Text = "";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                parentAdapter.SelectCommand = new SqlCommand("select * from Echipaj;", connection);
+                // childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Jocuri", connection);
+                childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Marinar", connection);
+                parentAdapter.Fill(ds, "Echipaj");
+                childAdapter.Fill(ds, "Marinar");
+            }
+        }
+
+        private void gridMarinar_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var rows = gridMarinar.SelectedRows;
+            if (rows.Count == 0 || rows[0].Cells.Count == 0 || rows[0].Cells["cod_individ"].Value.ToString().Equals(""))
+            {
+                return;
+            }
+
+            cod_marinarText.Text = rows[0].Cells["cod_marinar"].Value.ToString();
+            functieText.Text = rows[0].Cells["functie"].Value.ToString();
+            aliasText.Text = rows[0].Cells["alias"].Value.ToString();
+            cod_individText.Text = rows[0].Cells["cod_individ"].Value.ToString();
+            cod_echipajText.Text = rows[0].Cells["cod_echipaj"].Value.ToString();
+        }
+
+        private void adauga_Click(object sender, EventArgs e)
+        {
+            var rows = gridEchipaj.SelectedRows;
+            if (rows.Count == 0 || rows[0].Cells.Count == 0 || rows[0].Cells["cod_echipaj"].Value.ToString().Equals(""))
+            {
+                MessageBox.Show("Alegeti un echipaj!");
+                return;
+            }
+
+            int echipaj = int.Parse(rows[0].Cells["cod_echipaj"].Value.ToString());
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand insertCommand = new SqlCommand("insert into Marinar (functie,alias,cod_individ,cod_echipaj,cod_marinar)values(@functie, @alias,@cod_individ,@cod_echipaj,@cod_marinar);", connection);
+                insertCommand.Parameters.AddWithValue("@functie", functieText.Text);
+                insertCommand.Parameters.AddWithValue("@alias", aliasText.Text);
+                insertCommand.Parameters.AddWithValue("@cod_individ", int.Parse(cod_individText.Text));
+                insertCommand.Parameters.AddWithValue("@cod_echipaj", echipaj);
+                insertCommand.Parameters.AddWithValue("@cod_marinar", int.Parse(cod_marinarText.Text));
+                int inserted = insertCommand.ExecuteNonQuery();
+                MessageBox.Show("Numarul marinarilor inserati: " + inserted);
             }
         }
     }
